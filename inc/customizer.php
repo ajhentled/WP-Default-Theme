aabaa8f34b917d496d5705c097f4438adaceed01
cyk5w84cyw <jhentle.anamong@gmail.com>
2017-03-23 23:22:47 -0700

    Install components.underscores

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * WP Default - Components Theme Customizer
+ *
+ * @package WP_Default_-_Components
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function scwd_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'scwd_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function scwd_customize_preview_js() {
+	wp_enqueue_script( 'scwd_customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'scwd_customize_preview_js' );
