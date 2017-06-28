271326c3f7f1409e1db91f419ddf666258a858cc
Jhentle Anamong <ajhentled@gmail.com>
2017-06-29 00:45:45 +0800

    Rename extras.php to template-functions.php

--- functions.php
+++ functions.php
@@ -295,7 +295,7 @@ require get_template_directory() . '/inc/template-tags.php';
 /**
  * Custom functions that act independently of the theme templates.
  */
-require get_template_directory() . '/inc/extras.php';
+require get_template_directory() . '/inc/template-functions.php';
 
 /**
  * Customizer additions.
